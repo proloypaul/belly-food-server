38827be9a15207b3f128ea998e1f44e47ba02eb8
proloypaul <proloyp23@gmail.com>
2023-03-22 14:12:47 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb');
 const express = require("express");
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 3600;
 
 // mongodb connected link 
-// const uri = "mongodb://<username>:<password>@cluster0-shard-00-00.e3dsx.mongodb.net:27017,cluster0-shard-00-01.e3dsx.mongodb.net:27017,cluster0-shard-00-02.e3dsx.mongodb.net:27017/?ssl=true&replicaSet=atlas-cjr3cu-shard-0&authSource=admin&retryWrites=true&w=majority";
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.e3dsx.mongodb.net:27017,cluster0-shard-00-01.e3dsx.mongodb.net:27017,cluster0-shard-00-02.e3dsx.mongodb.net:27017/?ssl=true&replicaSet=atlas-cjr3cu-shard-0&authSource=admin&retryWrites=true&w=majority`;
+// console.log("conected link", uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('belly-food');
+        const foodscollection = database.collection('foods');
+    }finally{
+        // await client.close()
+    }
+}
 
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send(`server running successfully port: ${port}`);
 })
